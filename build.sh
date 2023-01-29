dd9438d62b0be4d76a9148adc813fa678165b4ce
masaya kato <masayakato@masayanoMacBook-Air.local>
2023-01-29 23:10:23 +0900

    deploy bugfix no collectstatic

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --settings=config.settings.production
 python manage.py migrate
 python manage.py createsuperuser --username admin --email admin@test.com --noinput
